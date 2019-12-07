7a28b1b4b39fe9db619530d4c4365bc07a8319bb
Luc de wit <luc.dewit@hotmail.nl>
2019-12-07 22:36:45 +0100

    finished leaderboard parser

--- commands.js
+++ commands.js
@@ -1,61 +1,53 @@
-let json;
-let months = ["JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"]
-function add(name, time, moves, link){
-	submit ={
-		"name": name,
-		"time": time,
-		"date": new Date().getFullYear()+"-"+new Date().getMonth()+"-"+new Date().getDay(),
-		"evidence": link,
-		"comments": moves+" moves"
-	}
-	json[name] = submit;
-}
-
-function load(){
-	$.getJSON('data/5x5.json', function(data) {
-    	json = data;
-    	loadall()
+//handeling loading the record table----------------------------------------------------------------------
+function load(type="5x5 single"){
+	fetch('data/'+type+'.json').then(res=>res.json()).then(data=>{
+		const json = data;
+		for (const elem in json){
+			build(json[elem]);
+		}
 	});
 }
 
 function build(obj){
-	let table = document.getElementById("table");
-	let row = table.insertRow(-1);
+	const table = document.getElementById("table");
+	const row = table.insertRow(-1);
 
-	let namecell = row.insertCell(0);
-	let timecell = row.insertCell(1);
-	let datecell = row.insertCell(2);
-	let evidencecell = row.insertCell(3);
-	let commentcell = row.insertCell(4);
-
+	const namecell = row.insertCell(0);
+	const timecell = row.insertCell(1);
+	const datecell = row.insertCell(2);
+	const evidencecell = row.insertCell(3);
+	const commentcell = row.insertCell(4);
 
 	namecell.innerHTML = obj.name;
 	timecell.innerHTML = obj.time;
 	datecell.innerHTML = obj.date;
 
-	let a = document.createElement('a');
-	var linkText = document.createTextNode("Link");
-	a.appendChild(linkText);
-	a.title = "Link"
+	const linkText = document.createTextNode("Link");
+	const a = document.createElement('a').appendChild(linkText);
+	
+	a.title = "Link";
 	a.href = obj.evidence;
 	evidencecell.appendChild(a);
-	commentcell.innerHTML = obj.comments
+	commentcell.innerHTML = obj.comments;
 }
 
-function len(jso){
-	counter = 0;
-	for (elem in jso){
-		counter++;
-	}
-	return counter;
-}
-
-function loadall(){
-	for (elem in json){
-		console.log(elem)
-		build(json[elem])
+function tableClear(){
+	const rows = document.getElementById("table").children[0].children.length;
+	for (let i = 0; i < rows-1; i++){
+		document.getElementById("table").children[0].children[1].remove();
 	}
 }
 
-load()
-//setTimeout(function(){loadall()}, 1000); 
+//handeling the form input----------------------------------------------------------------------------------
+function changeEvent(){
+	const event = document.getElementById("dropdown").value;
+
+	//change title
+	document.getElementById("event").innerText = event;
+
+	//clear table
+	tableClear()
+
+	//load new data
+	load(event);
+}
