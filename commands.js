08eed5457879f903f7b0504aba0c0cad9134d08a
Luc de wit <luc.dewit@hotmail.nl>
2019-12-08 15:00:15 +0100

    improved json parsing

--- commands.js
+++ commands.js
@@ -2,33 +2,42 @@
 function load(type="5x5 single"){
 	fetch('data/'+type+'.json').then(res=>res.json()).then(data=>{
 		const json = data;
+		let rank = 1
 		for (const elem in json){
-			build(json[elem]);
+			build(json[elem], rank);
+			rank+=1
 		}
 	});
 }
 
-function build(obj){
+function build(obj, rank){
 	const table = document.getElementById("table");
 	const row = table.insertRow(-1);
+	
+	const rankcell = row.insertCell(0)
+	const namecell = row.insertCell(1);
+	const timecell = row.insertCell(2);
+	const datecell = row.insertCell(3);
+	const evidencecell = row.insertCell(4);
+	const commentcell = row.insertCell(5);
 
-	const namecell = row.insertCell(0);
-	const timecell = row.insertCell(1);
-	const datecell = row.insertCell(2);
-	const evidencecell = row.insertCell(3);
-	const commentcell = row.insertCell(4);
-
+	rankcell.innerHTML = rank
 	namecell.innerHTML = obj.name;
 	timecell.innerHTML = obj.time;
 	datecell.innerHTML = obj.date;
-
-	const linkText = document.createTextNode("Link");
-	const a = document.createElement('a').appendChild(linkText);
+	evidencecell.innerHTML = "<a href="+obj.evidence+">Link</a>"
+	// const linkText = document.createTextNode("Link");
+	// const a = document.createElement('a').appendChild(linkText);
 	
-	a.title = "Link";
-	a.href = obj.evidence;
-	evidencecell.appendChild(a);
-	commentcell.innerHTML = obj.comments;
+	// a.title = "Link";
+	// a.href = obj.evidence;
+	// evidencecell.appendChild(a);
+	console.log(obj.comments)
+	if (obj.comments != "/ moves"){
+		commentcell.innerHTML = obj.comments;
+	}else{
+		commentcell.innerHTML = "/"
+	}
 }
 
 function tableClear(){
